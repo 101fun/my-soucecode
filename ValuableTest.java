648862cc2a611d0087e69e8d3a0755b0b9d1a01a
101fun <kazuma_14raita@ezweb.ne.jp>
2015-03-02 12:18:08 +0900

    20150302

---
+++ ValuableTest.java
@@ -0,0 +1,11 @@
+public class ValuableTest{
+	public static void main(String[] args){
+		int age = 35;
+		System.out.print("age:");
+		System.out.println(age);
+		age = age + 1; //int=age
+		System.out.println("一年後…");
+		System.out.print("age;");
+		System.out.print(age);
+	}
+}
